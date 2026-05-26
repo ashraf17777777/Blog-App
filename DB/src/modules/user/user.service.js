3cc9f34dd89392b346ceb2dd13d04d4746c50439
ashraf17777777 <ashrafdya663@gmail.com>
2026-05-26 05:46:41 +0200

    adding blog project ... after sequilize 3

--- DB/src/modules/user/user.service.js
+++ DB/src/modules/user/user.service.js
@@ -2,25 +2,63 @@ import { db } from "../../../db/connect.js";
 import { Person } from "../../../db/models/user.model.js";
 import { Blog } from "../../../db/models/blog.model.js";
 
-// Get User 
-export const getUserLogic = async (req,res){
-    try{
-        const {UserId} = req.params;
-  const user = Person.findByPk(UserId)
-  if(!user) return res.status(404).json({message:"User not found"})
-  res.status(200).json({message: "User Found",user})
-    } catch(error){
-       res.status(500).json({message: error.message})
-    }
-}
+// Get User
+export const getUserLogic = async (req, res) => {
+  try {
+    const { UserId } = req.params;
+    const user = await Person.findByPk(UserId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.status(200).json({ message: "User Found", user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 // Update User
+export const updateUserLogic = async (req, res) => {
+  try {
+    const { UserId } = req.params;
+    const { firstName } = req.body;
+    if (!firstName)
+      return res.status(400).json({ message: "First name is required!" });
+    const user = await Person.findByPk(UserId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    const updatedUser = await user.update({ firstName });
+    res.status(200).json({ message: "User Updated", user: updatedUser });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 // Delete User
+export const deleteUserLogic = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = Person.findByPk(id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    const deletedUser = await user.destroy();
+    res.status(200).json({ message: "User Deleted", user: deletedUser });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 // Search Users by Name
-
-
+export const searchUserLogic = async (req, res) => {
+  try {
+    const { firstName } = req.body;
+    const users = await Person.findAll({
+      where: {
+        firstName: {
+          [Sequelize.Op.like]: `${firstName}%`,
+        },
+      },
+    });
+    res.status(200).json({ message: "Users Found", users });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 // // 1. جلب بيانات مستخدم معين (Get User)
 // export const getUserLogic = (req, res) => {
